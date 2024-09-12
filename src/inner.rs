a62cb9b006a80c1e719b39e5c6ab2f0c9da5a607
Mark LeMoine <linclelinkpart5@gmail.com>
2024-09-12 00:17:27 -0700

    Add `to_f32` method on `Inner`

--- src/inner.rs
+++ src/inner.rs
@@ -2,3 +2,16 @@ enum Inner {
     Max,
     Sub(u32),
 }
+
+impl Inner {
+    fn to_f32(self) -> f32 {
+        match self {
+            Self::Max => 1.0,
+            Self::Sub(0) => 0.0,
+            Self::Sub(n) => {
+                let den: f32 = (u32::MAX as f32) + 1.0;
+                n as f32 / den
+            }
+        }
+    }
+}
